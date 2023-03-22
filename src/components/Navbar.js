cb8359e7040d713877ba8c3d53537c53168ba430
Alley Harmon <112970302+GOODSTROKE@users.noreply.github.com>
2023-03-22 10:19:22 -0400

    Boiler update

---
+++ src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Navbar() {
+  return (
+    <div className='fixed top-0 left-0 text-center w-full header bg-violet-600 py-4 text-white font-bold text-lg shadow-lg'>
+      Full Stack Todo By React Node Express and MongoDB
+    </div>
+  )
+}
